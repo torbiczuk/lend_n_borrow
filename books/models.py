d10107fb2d4a3d1d682cdd6b608c39e394e114bd
Robert <trobert93@gmail.com>
2018-06-14 17:23:10 +0200

    add book "main" app

---
+++ books/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+from books.constants import RATING_VALUES
+
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.CharField(max_length=70)
+
+
+class Library(models.Model):
+    owner = models.ForeignKey(Person, verbose_name='Właściciel', on_delete=models.CASCADE, related_name='owner')
+    book = models.ForeignKey(Book, verbose_name='Książka', on_delete=models.DO_NOTHING)
+    borrowed = models.ForeignKey(Person, verbose_name='Pożyczono dla', on_delete=models.SET_DEFAULT,
+                                 default='', related_name='borrowed')
+
+
+class Rating(models.Model):
+    owner = models.ForeignKey(Person, verbose_name='Autor oceny', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    rate = models.CharField(max_length=1, choices=RATING_VALUES, default='')
+
+
+class BookRating(Rating):
+    book = models.ForeignKey(Book, verbose_name='Książka', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Autor oceny: {} Książka: {} Ocena: {}'.format(self.owner, self.book, self.rate)
+
+
+class UserRating(Rating):
+    person = models.ForeignKey(Book, verbose_name='Użytkownik', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Autor oceny: {} Ocena: {} Oceniany: {}'.format(self.owner, self.rate, self.person)
